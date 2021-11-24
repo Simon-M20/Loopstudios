6fdc32cba13c2a682903b982d6258e5d2366db61
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2021-11-23 21:20:34 -0400

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,20 @@
-const menuBtn = document.getElementById("menu-btn")
-
+const menuBtn = document.querySelector(".menu-btn")
+const menu = document.querySelector('.menu');
+const menuLink = document.querySelectorAll('.menu-link')
 console.log(menuBtn);
 
 document.addEventListener("click", e => {
-    if (e.target.matches(menuBtn)) {
-        console.log(e.target);
+    if (e.target === menuBtn) {
+        // console.log(e.target);
+        menu.classList.toggle('show');
+        menuBtn.classList.toggle('is-active')
     }
+})
+
+menuLink.forEach(el => {
+    el.addEventListener('click', e => {
+        console.log(e.target);
+        menu.classList.remove('show');
+        menuBtn.classList.remove('is-active')
+    })
 })
